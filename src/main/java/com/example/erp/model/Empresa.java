20ffb41a21ac9345962370a123a0f6d2cdd08875
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-06-24 16:02:15 -0300

    Validacao com Bean Validation

--- src/main/java/com/example/erp/model/Empresa.java
+++ src/main/java/com/example/erp/model/Empresa.java
@@ -1,8 +1,13 @@
 package com.example.erp.model;
 
+import org.hibernate.validator.constraints.br.CNPJ;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -15,27 +20,35 @@ public class Empresa implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     @Column(name = "nome_fantasia", nullable = false, length = 80)
     private String nomeFantasia;
 
+    @NotEmpty
     @Column(name = "razao_social", nullable = false, length = 120)
     private String razaoSocial;
 
+    @CNPJ
+    @NotNull
     @Column(nullable = false, length = 18)
     private String cnpj;
 
+    @NotNull
+    @Past
     @Temporal(TemporalType.DATE)
     @Column(name = "data_fundacao")
     private Date dataFundacao;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "ramo_atividade_id", nullable = false)
     private RamoAtividade ramoAtividade;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false,  length = 30)
     private TipoEmpresa tipo;
-    
+
     public Long getId() {
         return id;
     }
