4e26fecebf04c793e996e792e30e03ea046bd461
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-06-23 23:55:37 -0300

    Listando empresas com tabela de dados

--- src/main/java/com/example/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/example/erp/controller/GestaoEmpresasBean.java
@@ -1,36 +1,30 @@
 package com.example.erp.controller;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
+import com.example.erp.model.Empresa;
+import com.example.erp.repository.Empresas;
+
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import com.example.erp.model.Empresa;
-import com.example.erp.model.TipoEmpresa;
+import java.util.List;
 
-@Named
+
+@Named("gestaoEmpresasBean")
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private Empresa empresa = new Empresa();
+    private List<Empresa> listaEmpresas;
 
-    public Empresa getEmpresa() {
-        return empresa;
+    @Inject
+    private Empresas empresas;
+
+    public void todasEmpresas(){
+        listaEmpresas = empresas.todas();
     }
 
-    public TipoEmpresa[] getTiposEmpresa(){
-        return TipoEmpresa.values();
-    }
-
-    public void salvar(){
-        System.out.println("Razao social: " + empresa.getRazaoSocial()
-                            + "Nome fantasia: " + empresa.getNomeFantasia()
-                            + "Tipo: " + empresa.getTipo());
-    }
-
-    public String ajuda(){
-        return "AjudaGestaoEmpresas?faces-redirect=true";
+    public List<Empresa> getListaEmpresas() {
+        return listaEmpresas;
     }
 }
